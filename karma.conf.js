5fa415e3ebd2cb9615d147bd2963e4d39b697888
Tim Burke <timothy.e.burke@gmail.com>
2015-10-18 14:42:38 -0400

    Clean up karma conf.

--- karma.conf.js
+++ karma.conf.js
@@ -1,77 +1,27 @@
-// Karma configuration
-// Generated on Sun Oct 18 2015 12:55:46 GMT-0400 (EDT)
-
 module.exports = function(config) {
-  config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      'node_modules/angular/angular.min.js',
-      'node_modules/angular-mocks/angular-mocks.js',
-      'assets/scripts/**/*.js',
-      'tests/**/*.test.js'
-    ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
-
-    mochaReporter: {
-      colors: {
-        success: 'green',
-        info: 'bgGreen',
-        warning: 'cyan',
-        error: 'bgRed'
-      }
-    },
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  })
-}
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            'node_modules/angular/angular.min.js',
+            'node_modules/angular-mocks/angular-mocks.js',
+            'assets/scripts/**/*.js',
+            'tests/**/*.test.js'
+        ],
+        reporters: ['mocha'],
+        mochaReporter: {
+            colors: {
+                success: 'green',
+                info: 'bgGreen',
+                warning: 'cyan',
+                error: 'bgRed'
+            }
+        },
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: true
+    })
+};
